b929fb8779dae51f8f5a47b6fe08de163c20934e
aristo <dkfud2121@naver.com>
2024-04-19 13:29:55 +0900

    feat: create spring project

---
+++ Mytube/src/main/java/com/flab/Mytube/MytubeApplication.java
@@ -0,0 +1,13 @@
+package com.flab.Mytube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytubeApplication.class, args);
+	}
+
+}
---
+++ Mytube/src/test/java/com/flab/Mytube/MytubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.Mytube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
