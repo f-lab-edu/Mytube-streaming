8d4e0e34bdf3e4a629ab839c6dbf7da81c166516
aristo <dkfud2121@naver.com>
2024-04-25 12:41:00 +0900

    doc : add memo for mybatis

--- Mytube/src/main/java/com/flab/Mytube/config/DatabaseConfig.java
+++ Mytube/src/main/java/com/flab/Mytube/config/DatabaseConfig.java
@@ -7,45 +7,17 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @SpringBootConfiguration
-//@PropertySource("classpath:/application.properties")
 @MapperScan(value = "com.flab.Mytube", sqlSessionFactoryRef = "sqlSessionFactory")
 public class DatabaseConfig {
-//    @Value("${spring.datasource.mapper-location}")
-//    String mPath;
-
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource(){
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource DataSource, ApplicationContext applicationContext) throws Exception{
-//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-//        sqlSessionFactoryBean.setDataSource(DataSource);
-//        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResource(mPath));
-//        return sqlSessionFactoryBean.getObject();
-//    }
-//
-//    @Bean(name="SessionTemplate")
-//    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory){
-//        return new SqlSessionTemplate(firstSqlSessionFactory);
-//    }
-
     @Autowired
     private ApplicationContext applicationContext;
 
@@ -55,6 +27,7 @@ public class DatabaseConfig {
         return new HikariConfig();
     }
 
+    //SqlSession을 구현하고 코드에서 SqlSession를 대체
     SqlSessionTemplate sqlSessionTemplate;
     public SqlSessionTemplate getSqlSessionTemplate(){
         return sqlSessionTemplate;
@@ -69,6 +42,9 @@ public class DatabaseConfig {
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        //SqlSessionFactoryBean은 SqlSessionFactory를 만들기 위해 사용
+        //SqlSessionFactoryBean 은 스프링의 FactoryBean 인터페이스를 구현
+
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
 //        sqlSessionFactoryBean.setTypeAliasesPackage("board.board.dto");
