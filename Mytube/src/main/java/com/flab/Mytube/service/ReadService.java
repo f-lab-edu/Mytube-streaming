5d9fff3bead933486debffdb205b4c3c0b474a84
aristo <dkfud2121@naver.com>
2024-05-20 20:49:19 +0900

    feat: delete API controller, Service 코드 연결, API 경로 생각해보기

--- Mytube/src/main/java/com/flab/Mytube/service/ReadService.java
+++ Mytube/src/main/java/com/flab/Mytube/service/ReadService.java
@@ -1,15 +1,20 @@
 package com.flab.Mytube.service;
 
+import com.flab.Mytube.mapper.ReadMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReadService {
+    private final ReadMapper readMapper;
         //라이브 삭제
         //    관련 라이브 테이블에서 deletedAt 날짜 추가하기
         //    조회하는 코드에서 이 부분에 값이 있다면 불러오지 못하게 처리할 것
-    public void delete(long streamerId, long movieId){ //dto 로 만들기
+    public void delete(long id){ //dto 로 만들기
 //        ----- id= liveDelete
 //        UPDATE liveStreaming set deletedAt = CURRENT_TIMESTAMP where id=3;
+        readMapper.liveDelete(id);
     }
 
     //현재 채널 지난 라이브 다시보기
