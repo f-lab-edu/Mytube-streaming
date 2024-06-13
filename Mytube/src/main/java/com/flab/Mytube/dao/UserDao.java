b747354f66bc7fbe38de4378d4de40c4400384b5
aristo <dkfud2121@naver.com>
2024-06-13 20:54:27 +0900

    refactor: 불필요한 코드 삭제

--- Mytube/src/main/java/com/flab/Mytube/dao/UserDao.java
+++ Mytube/src/main/java/com/flab/Mytube/dao/UserDao.java
@@ -1,4 +1,15 @@
 package com.flab.Mytube.dao;
 
-public interface UserDao {
+import com.flab.Mytube.vo.User;
+import org.apache.ibatis.session.SqlSession;
+
+public class UserDao {
+    private SqlSession sqlSession;
+    public void setSqlSession(SqlSession sqlSession){
+        this.sqlSession=sqlSession;
+    }
+
+    public User getUser(String userId){
+        return sqlSession.selectOne("org.mybatis.spring.sample.mapper.UserMapper.getUser", userId);
+    }
 }
