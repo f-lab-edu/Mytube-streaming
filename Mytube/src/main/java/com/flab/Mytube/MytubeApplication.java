4f8c58014b428451180874e91da7a263b9c5aa90
aristo <dkfud2121@naver.com>
2024-06-20 21:24:32 +0900

    test: kafka 최초의 연결 성공

--- Mytube/src/main/java/com/flab/Mytube/MytubeApplication.java
+++ Mytube/src/main/java/com/flab/Mytube/MytubeApplication.java
@@ -1,18 +1,40 @@
 package com.flab.Mytube;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @EnableCaching
 @SpringBootApplication
 public class MytubeApplication {
-	private static final Log LOG = LogFactory.getLog(MytubeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MytubeApplication.class, args);
+	}
+
+	@KafkaListener(id = "graal", topics = "graal")
+	public void listen(String in) {
+		System.out.println("++++++Received:" + in);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("graal").partitions(1).replicas(1).build();
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			template.send("graal", "foo");
+			System.out.println("++++++Sent:foo");
+			Thread.sleep(5000);
+		};
 	}
 
 }
---
+++ Mytube/src/main/java/com/flab/Mytube/config/KafkaProducerConfig.java
@@ -0,0 +1,31 @@
+//package com.flab.Mytube.config;
+//
+//import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.ProducerFactory;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaProducerConfig {
+//
+//    @Bean
+//    public ProducerFactory<String, Object> producerFactory() {
+//        Map<String, Object> config = new HashMap<>();
+//        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(config);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, Object> kafkaTemplate() {
+//        return new KafkaTemplate<>(producerFactory());
+//    }
+//}
