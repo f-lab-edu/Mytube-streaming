3c55400fe4e40384ac064d4cc8572c4a26a6cb2a
aristo <dkfud2121@naver.com>
2024-06-25 01:42:57 +0900

    refactor: 불필요한 import 문 제거

--- Mytube/src/main/java/com/flab/Mytube/MytubeApplication.java
+++ Mytube/src/main/java/com/flab/Mytube/MytubeApplication.java
@@ -1,7 +1,5 @@
 package com.flab.Mytube;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
