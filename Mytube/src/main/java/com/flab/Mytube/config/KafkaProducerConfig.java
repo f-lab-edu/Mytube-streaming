8646886b492d92c6f6a61da422d1bdbb7da477f5
aristo <dkfud2121@naver.com>
2024-06-26 20:53:45 +0900

    fix: 브랜치 정리

--- Mytube/src/main/java/com/flab/Mytube/config/KafkaProducerConfig.java
+++ Mytube/src/main/java/com/flab/Mytube/config/KafkaProducerConfig.java
@@ -1,7 +1,6 @@
 package com.flab.Mytube.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- Mytube/src/main/java/com/flab/Mytube/kafka/Producer.java
+++ Mytube/src/main/java/com/flab/Mytube/kafka/Producer.java
@@ -1,37 +1,19 @@
 package com.flab.Mytube.kafka;
 
 import java.util.concurrent.CompletableFuture;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.messaging.Message;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
-//@RequiredArgsConstructor
 @Slf4j
 public class Producer {
 
   @Autowired
   private KafkaTemplate<String, String> template;
-
-//  public void sendToKafka(final MyOutputData data) {
-//    final ProducerRecord<String, String> record = createRecord(data);
-//
-//    CompletableFuture<SendResult<Integer, String>> future = template.send((Message<?>) record);
-//    future.whenComplete((result, ex) -> {
-//      if (ex == null) {
-//        handleSuccess(data);
-//      } else {
-//        handleFailure(data, record, ex);
-//      }
-//    });
-//  }
 
   public void sendMessage(final MyOutputData data) {
     ProducerRecord<String, String> record = createRecord(data);
