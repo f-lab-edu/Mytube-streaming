8fa6f8e2a09db54b6e6d511152737f8b7039f6ed
aristo <dkfud2121@naver.com>
2024-07-01 15:34:06 +0900

    feat: kafka 에 객체 전달하여 listener 에서 수신하기

--- Mytube/src/main/java/com/flab/Mytube/kafka/EncodingRequest.java
+++ Mytube/src/main/java/com/flab/Mytube/kafka/EncodingRequest.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @Builder
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class EncodingRequest implements Serializable {
   String topic;
