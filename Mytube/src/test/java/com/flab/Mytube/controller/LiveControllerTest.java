e2ac165a623c68008ce2980c8c3d15ece0eaf370
aristo <dkfud2121@naver.com>
2024-07-16 13:12:24 +0900

    feat: LiveController Test code

--- Mytube/src/test/java/com/flab/Mytube/controller/LiveControllerTest.java
+++ Mytube/src/test/java/com/flab/Mytube/controller/LiveControllerTest.java
@@ -2,23 +2,31 @@ package com.flab.Mytube.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flab.Mytube.domain.LiveStreaming;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-class ChannelControllerTest {
+class LiveControllerTest {
+
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   MockMvc mockMvc;
   @Autowired
@@ -31,29 +39,31 @@ class ChannelControllerTest {
         .build();
   }
 
+
   @Test
-  @DisplayName("user Id:3 이 게시한 라이브 목록 조회 Test")
-  void getLiveList() throws Exception {
+  @DisplayName("라이브 시작 Test")
+  public void getReservation() throws Exception {
+    String title = "TEST CODE RESERVE";
+    String contents = "HI";
+    String inputString = "2024-08-21T13:30";
+    DateTimeFormatter parser = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+    LocalDate reservedAt = LocalDate.parse(inputString, parser);
+
+    LiveStreaming streaming = LiveStreaming.builder()
+        .channelId(3)
+        .movieId(179)
+        .title(title)
+        .contents(contents)
+        .reservedAt(reservedAt)
+        .build();
+
     this.mockMvc
-        .perform(get("/api/v1/channels/3"))
+        .perform(post("/api/v1/lives")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(streaming)
+            ))
         .andExpect(status().isOk())
         .andDo(print());
   }
 
-  @Test
-  @DisplayName("LiveId:22 정보 조회 Test")
-  void replayLive() throws Exception {
-    this.mockMvc
-        .perform(get("/api/v1/channels/lives/22/replay"))
-        .andExpect(status().isOk())
-        .andDo(print());
-  }
-  @Test
-  @DisplayName("삭제된 LiveId:15 정보 조회 Test")
-  void replayDeletedLive() throws Exception {
-    this.mockMvc
-        .perform(get("/api/v1/channels/lives/15/replay"))
-        .andExpect(status().isNotFound())
-        .andDo(print());
-  }
 }
